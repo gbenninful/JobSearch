1ffc2be98094acef9cd91e4c38da2679223acb53
gbenninful <enninful@hotmail.com>
2014-04-15 08:06:08 -0700

    Linked up model using Revealing Module Pattern

---
+++ script/jobsearch.js
@@ -0,0 +1,134 @@
+﻿var JobSearchApp = JobSearchApp || {};
+
+
+$(function () {
+    "use strict";
+
+    var MobileServiceClient = WindowsAzure.MobileServiceClient,
+        client = new MobileServiceClient("https://techjobs.azure-mobile.net/", "yMHpVaLVZGcfABWDybwMWbljdIMMSd81"),
+        jobPostingTable = client.getTable("jobPosting"),
+        addressTable = client.getTable("address"),
+        companyInfoTable = client.getTable("companyInfo");
+
+    //Job Posting
+    var $title = $("#job-title"),
+        $cat = $("input:radio[name=job-category]:checked"),
+        $jid = $("#job-id"),
+        $description = $("#job-description"),
+        $jobEmail = $("#email-resume");
+
+    //Company Info
+    var compName = $("#company-name"),
+        compEmail = $("#company-email"),
+        compUrl = $("#company-url"),
+        cid = $("#company-id"),
+        tel = $("#phone");
+
+    //Company Address
+    var streetAd = $("#street"),
+        cityName = $("#city"),
+        stateName = $("#state"),
+        zip = $("#zip-code");
+
+    $("#job-form").submit(function (e) {
+
+        //var jobTitle = $title.val(),
+        //    cat = $cat.val(),
+        //    jobId = $jid.val(),
+        //    jobDescription = $description.val(),
+        //    hrEmail = $jobEmail.val();
+
+       
+
+
+
+        var name = compName.val(),
+            companyEmail = compEmail.val(),
+            url = compUrl.val(),
+            companyId = cid.val(),
+            phone = tel.val(),
+            street = streetAd.val(),
+            city = cityName.val(),
+            state = stateName.val(),
+            zipCode = zip.val();
+
+        //Insert Data into Tables
+
+        //Insert company first, so i can get the companyId to use to establish my foreign key on the job posting and the address
+
+         var cmp = {
+             name: name,
+             companyEmail: companyEmail,
+             url: url,
+             phone: phone
+         };
+
+         toastr.warning("About to save some data");
+
+         companyInfoTable.insert(cmp).then(function (savedCompany) {
+
+             //create job posting
+             postJob(savedCompany.id);
+
+             //save address
+             postAddress(savedCompany.id);
+
+         }, function (err) {
+             console.log(err);
+         })
+
+         function postJob(companyId) {
+
+             var jobPosting = new JobSearchApp.Models.JobPosting({
+                 jobTitle: $title.val(),
+                 category: $cat.val(),
+                 jobId: $jid.val(),
+                 jobDescription: $description.val(),
+                 hrEmail: $jobEmail.val(),
+                 companyId: companyId
+
+             });
+
+             jobPostingTable.insert(jobPosting).then(function (savedJob) {
+                 toastr.success("Your company data was Successful");
+                 console.log(savedJob);
+             });
+
+         }
+
+        function postAddress(companyId){
+
+             addressTable.insert({
+                        street: street,
+                        city: city,
+                        state: state,
+                        zipCode: zipCode,
+                        companyId: companyId
+                    }).then(function (savedAddress) {
+                            toastr.success("Your address was Successful");
+                            console.log(savedAddress);
+                    }, function (error) {
+                        toastr.error("Error saving your address " + error);
+                    });
+         }
+        e.preventDefault();
+    });
+
+
+}());
+
+//authorInfoTable.insert({
+//    bookTitle: bookTitle,
+//    firstName: firstName,
+//    lastName: lastName,
+//    gender: gender
+//})
+//           .then(function (data) {
+//               toastr.success("Your post was successful");
+//           }, function (error) {
+//               toastr.error("Your error is " + error);
+//           });
+
+//clearForm();
+
+//e.preventDefault();
---
+++ script/models.js
@@ -0,0 +1,42 @@
+﻿"use strict";
+var JobSearchApp = JobSearchApp || {};
+
+ JobSearchApp.Models =   (function () {
+   
+    //JOB POSTING
+    var JobPosting = function (options) {
+        options = options || {};
+        this.companyId = companyId,
+
+        this.jobTitle = options.jobTitle || "";
+        this.category = options.category || "";
+        this.jobId = options.jobId || "";
+        this.jobDescription = options.jobDescription || "";
+        this.hrEmail = options.hrEmail || "";
+    };
+
+    //ADDRESS
+    var Address = function (street, city, state, zipCode) {
+        this.companyId = companyId,
+
+        this.street = street,
+        this.city = city,
+        this.state = state,
+        this.zipCode = zipCode
+    };
+
+    //COMPANY
+    var Company = function (name, companyEmail, phone, url) {
+        this.name = name,
+        this.companyEmail = companyEmail,
+        this.phone = phone,
+        this.url = url
+    };
+
+    return {
+        JobPosting: JobPosting,
+        Address: Address,
+        Company: Company
+    };
+
+})();
